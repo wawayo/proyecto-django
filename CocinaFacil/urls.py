78ee0651a04f15b6194dc0dfaac13832adaf95e4
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2023-11-12 15:09:13 -0500

    creacion de receta funciona - usuario loguado

--- CocinaFacil/urls.py
+++ CocinaFacil/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for CocinaFacil project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
@@ -27,7 +11,8 @@ urlpatterns = [
     path('', welcome_view, name='welcome'),
     path('recetas/', recetas_view, name='recetas'),
     path('recetas/crear/', receta_create, name='crear-receta'),
+    path('recetas/<int:id>/', receta_detail, name='detalle-receta'),
     path('admin/', admin.site.urls),
-] 
+]  
 #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
 # Para poder servir archivos estáticos en desarrollo
--- Recetario/views.py
+++ Recetario/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.template import loader
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
+from .models import *
 # Create your views here.
 
 def welcome_view(request):
@@ -8,5 +11,21 @@ def welcome_view(request):
 def recetas_view(request):
     return render(request, 'Recetario/recetas.html')
 
+def receta_detail(request, id):
+    receta = Receta.objects.get(id=id)
+    return render(request, 'Recetario/receta_detail.html', {'receta': receta})
+
 def receta_create(request):
-    return render(request, 'Recetario/crear-receta.html')
+    if request.method == 'POST':
+        titulo = request.POST['titulo']
+        ingredientes = request.POST['ingredientes']
+        descripcion = request.POST['descripcion']
+        instrucciones = request.POST['instrucciones']
+        autor_id = request.POST['autor_id']
+        receta = Receta(titulo=titulo, ingredientes=ingredientes, descripcion=descripcion, instrucciones=instrucciones, autor_id=autor_id)
+        receta.save()
+        #imprimir por consola el objeto
+        print(request.POST)
+        return redirect('recetas')
+    else:
+        return render(request, 'Recetario/crear_receta.html')
